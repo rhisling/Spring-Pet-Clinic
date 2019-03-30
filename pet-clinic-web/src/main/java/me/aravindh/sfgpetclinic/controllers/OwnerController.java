ada857ae76bd86cbb8bcc81113f7167be77d0551
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-03-30 12:33:50 -0700

    Fixed broken links. Closes #45

--- pet-clinic-web/src/main/java/me/aravindh/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/me/aravindh/sfgpetclinic/controllers/OwnerController.java
@@ -22,4 +22,10 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
+
+    @RequestMapping("/find")
+    public String findOwner() {
+        return "notimplemented";
+    }
 }
